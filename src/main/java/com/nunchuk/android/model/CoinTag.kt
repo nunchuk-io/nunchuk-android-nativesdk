6faa2fd3200d9ae9d088a8221cc569c3fc6e43e1
Le Khac Thong <khacthong.le@shopee.com>
2023-03-20 21:59:17 +0700

    Update default value of `id` CoinTag

--- src/main/java/com/nunchuk/android/model/CoinTag.kt
+++ src/main/java/com/nunchuk/android/model/CoinTag.kt
@@ -4,4 +4,4 @@ import android.os.Parcelable
 import kotlinx.parcelize.Parcelize
 
 @Parcelize
-data class CoinTag(var id: Int = 0, var name: String = "", var color: String = "") : Parcelable
+data class CoinTag(var id: Int = -1, var name: String = "", var color: String = "") : Parcelable
