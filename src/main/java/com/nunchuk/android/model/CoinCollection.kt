5c112544aad42b390bffde96fde3430f774349cd
Le Khac Thong <khacthong.le@shopee.com>
2023-03-29 22:07:13 +0700

    Update CoinCollection method

--- src/main/java/com/nunchuk/android/model/CoinCollection.kt
+++ src/main/java/com/nunchuk/android/model/CoinCollection.kt
@@ -4,4 +4,4 @@ import android.os.Parcelable
 import kotlinx.parcelize.Parcelize
 
 @Parcelize
-data class CoinCollection(var id: Int = -1, var name: String = "") : Parcelable
+data class CoinCollection(var id: Int = -1, var name: String = "", var isAddNewCoin: Boolean = false, var isAutoLock: Boolean = false) : Parcelable
