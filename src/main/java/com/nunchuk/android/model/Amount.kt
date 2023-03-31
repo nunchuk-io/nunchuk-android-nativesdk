48aeb58841c22dcd70bdadd8f8f8391c6c5cafb3
Jayce Luong <jayce@nunchuk.io>
2023-04-01 06:41:43 +0700

    Support amount plus and minus

--- src/main/java/com/nunchuk/android/model/Amount.kt
+++ src/main/java/com/nunchuk/android/model/Amount.kt
@@ -30,5 +30,8 @@ data class Amount(var value: Long = 0, var formattedValue: String = "0.00") : Pa
     }
 
     operator fun plus(amount: Amount) = Amount(value + amount.value)
+    operator fun plusAssign(amount: Amount) {
+        value += amount.value
+    }
     operator fun minus(amount: Amount) = Amount(value - amount.value)
 }
