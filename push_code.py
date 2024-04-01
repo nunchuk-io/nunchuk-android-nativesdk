52fb981d595c80dd2058d481d2dd3ba5adcf818c
Jayce Luong <jayce@nunchuk.io>
2024-04-01 21:41:08 +0700

    Implement auto release script

--- push_code.py
+++ push_code.py
@@ -5,6 +5,7 @@ def merge_and_push():
     # Set up paths and branch names
     master_branch = 'master'
     main_branch = 'main'
+    commit_message = 'Merge changes from master branch'
 
     # Fetch latest changes from GitLab
     subprocess.run(['git', 'fetch', 'origin'])
@@ -22,7 +23,9 @@ def merge_and_push():
     subprocess.run(['git', 'checkout', main_branch])
 
     # Merge the master branch into the main branch
-    subprocess.run(['git', 'merge', 'origin/master'])
+    subprocess.run(['git', 'merge', '--no-ff', '-m', commit_message, 'origin/master'])
 
+    # Push changes to the main branch on GitHub
+    # subprocess.run(['git', 'push', 'origin-github', main_branch])
 
 merge_and_push()
