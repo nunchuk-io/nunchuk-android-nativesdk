7eae77083df0bfdb24bd53d6ccda841bfe0ac2d7
Jayce Luong <jayce@nunchuk.io>
2025-01-19 14:40:40 +0700

    [GroupWallet] Add delete group listener

---
+++ src/main/java/com/nunchuk/android/listener/GroupDeleteListener.kt
@@ -0,0 +1,13 @@
+package com.nunchuk.android.listener
+
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.asSharedFlow
+
+object GroupDeleteListener {
+    private val _groupDeleteFlow = MutableSharedFlow<String>()
+    val groupDeleteFlow = _groupDeleteFlow.asSharedFlow()
+
+    fun onGroupDelete(groupId: String) {
+        _groupDeleteFlow.tryEmit(groupId)
+    }
+}
